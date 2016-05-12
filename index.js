2397f28392d790da5841ed08acba92682d343577
Doug Leary <Dleary4395@Gmail.com>
2016-05-12 15:55:58 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+var fs = require('fs');
+var path = require('path');
+var fileArray = [];
+var recursive = require('recursive-readdir');
+var SVGO = require('svgo');
+var svgo = new SVGO();
+var imagemin = require('imagemin');
+var imageminOptipng = require('imagemin-optipng');
+var imageminMozjpeg = require('imagemin-mozjpeg');
+var ProgressBar = require('progress');
+
+
+function getFiles(srcpath) {
+  recursive('./', function (err, files) {
+    // Files is an array of filename 
+    fileArray = files;
+   	iterateFiles(fileArray);
+  });
+}
+
+//Get file extension for file sent to function
+function getFileExtension(file) {
+  if((file.indexOf('.jpg') > -1 || file.indexOf('.svg') > -1 || file.indexOf('.png') > -1) && file.indexOf("node_modules") != 0) {
+	var fileParts = file.split('.');
+	switch(fileParts[1]) {
+      case 'svg':
+        fs.readFile(file, 'utf8', function(err, data) {
+          if (err) {
+              throw err;
+          }
+          svgo.optimize(data, function(result) {
+          	fs.writeFile(file, result.data, 'utf8');
+          	console.log(file + " has been optimized");
+          });
+        });
+        break;
+      case 'png':
+		imagemin([file], './', {use: [imageminOptipng()]}).then(() => {
+		    console.log(file + " has been optimized");
+		});
+		break;
+	  case 'jpg':
+	  case 'jpeg':
+	  	imagemin([file], './', {use: [imageminMozjpeg()]}).then(() => {
+		    console.log(file + " has been optimized");
+		});
+	  	break;
+    }
+    return true;
+  }
+  return false;
+}
+
+//Iterate through array of items given
+function iterateFiles(files) {
+  if(files) {
+  	var imageFiles = 0;
+    for(var i=0;i<files.length;i++) {
+      var file = files[i];
+      if(getFileExtension(file)) {
+      	imageFiles++;
+      }
+    }
+    if(imageFiles === 0) {
+    	console.log("Couldn't find any image files in this directory");
+    }
+  }
+}
+
+getFiles('');
